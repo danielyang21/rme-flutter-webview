c3cc448430b978367a288d7fd9843ee5952aad3e
danielyang21 <danielyanxiaoyang21@gmail.com>
2025-05-27 13:18:27 -0400

    Update compoundsServer.R

--- shinyapp/compoundsServer.R
+++ shinyapp/compoundsServer.R
@@ -648,7 +648,6 @@ observeEvent(input$uploadSubstances, {
 output$customTable <- renderDT({
   req(length(getTableData$result()) > 0)
   result <- getTableData$result()
-  colnames(result) <- iconv(colnames(result), from = "", to = "UTF-8")
   data <- result[, c("Name", 
                                 "Molecular Formula", 
                                 "Molecular Weight", 
