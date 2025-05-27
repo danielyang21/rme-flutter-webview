7facf566b07c7f357bb3ef2744e8b3d9d2832e04
danielyang21 <danielyanxiaoyang21@gmail.com>
2025-05-27 10:54:13 -0400

    making flutter app

---
+++ rme_app/android/app/src/main/kotlin/com/example/rme_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rme_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ rme_app/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+    late final WebViewController controller;
+    final String internalHost = 'danielyang21-rme-flutter-webview.share.connect.posit.cloud';
+
+    @override
+    void initState() {
+    super.initState();
+    controller = WebViewController()
+        ..setJavaScriptMode(JavaScriptMode.unrestricted)
+        ..setNavigationDelegate(
+        NavigationDelegate(
+            onNavigationRequest: (NavigationRequest request) async {
+            if (!request.url.contains('danielyang21-rme-flutter-webview.share.connect.posit.cloud')) {
+                final Uri uri = Uri.parse(request.url);
+                debugPrint('Intercepted URL: ${uri.toString()}');
+
+                if (uri.host != internalHost) {
+                    if (await canLaunchUrl(uri)) {
+                        await launchUrl(uri, mode: LaunchMode.externalApplication);
+                        return NavigationDecision.prevent;
+                    }
+                }
+            }
+            return NavigationDecision.navigate;
+            },
+        ),
+        )
+        ..loadRequest(Uri.parse('https://$internalHost'));
+    }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebViewWidget(controller: controller)
+    );
+  }
+}
+
